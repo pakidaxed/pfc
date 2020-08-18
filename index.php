31864c2bf7c77559faf562023dd39d568a1f3dab
pakidaxed <jucius.tomas@gmail.com>
2020-08-18 10:33:49 +0300

    tarpas po ;

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>As valdau HTML <?php print 'ir PHP';?> !</title>
+    <title>As valdau HTML <?php print 'ir PHP'; ?> !</title>
 </head>
 <body>
-<h1>Tomas - valdo HTML <?php print 'ir PHP';?> :D</h1>
+<h1>Tomas - valdo HTML <?php print 'ir PHP'; ?> :D</h1>
 <p>Belenkas belnkas belenkaip, laukiam php pagaliau kazkokio</p>
 <p><?php print date('Y-m-d H:i:s'); ?></p>
 </body>
