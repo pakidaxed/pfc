dbc2972f4e95e9877232718a3fc2bb706022309b
pakidaxed <jucius.tomas@gmail.com>
2020-08-18 11:04:27 +0300

    concatinaitinam

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>As valdau HTML <?php print 'ir PHP'; ?> !</title>
+    <title><?php print 'As, ' . date('d') . ' ir PHP'; ?> !</title>
 </head>
 <body>
-<h1>Tomas - valdo HTML <?php print 'ir PHP'; ?> :D</h1>
+<h1>Tomas - valdo HTML <?php print 'ir PHP jau nuo ' . date('Y') . 'metu.'; ?></h1>
 <p>Belenkas belnkas belenkaip, laukiam php pagaliau kazkokio</p>
-<p>Atidaryta: <?php print date('Y-m-d H:i:s'); ?></p>
+<p>Viskas prasidejo <?php print date('m') . ' menesio, ' . date('d') . 'diena'; ?>.</p>
 </body>
 </html>
