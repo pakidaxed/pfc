afb384c6d0b8605b64ceb77815d6f5ff40779f17
pakidaxed <jucius.tomas@gmail.com>
2020-08-20 10:16:49 +0300

    produktai array

--- array/index.php
+++ array/index.php
@@ -1,1 +1,20 @@
 <?php
+$produktai = [
+    [
+        'pavadinimas' => 'Grietinė',
+        'dydis' => 'big',
+        'kiekis' => 1
+    ],
+    [
+        'pavadinimas' => 'Baklažanas',
+        'dydis' => 'small',
+        'kiekis' => 1
+    ],
+    [
+        'pavadinimas' => 'Kiaušinis',
+        'dydis' => 'big',
+        'kiekis' => 2
+    ],
+];
+
+var_dump($produktai);
