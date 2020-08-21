e2e1b225d8a183aa2c58bbf76d7c547e657199e1
pakidaxed <jucius.tomas@gmail.com>
2020-08-21 15:46:33 +0300

    added score

--- foreach/index.php
+++ foreach/index.php
@@ -30,24 +30,25 @@ $game = [
     <meta charset="UTF-8">
     <title>THE GAME (foreach)</title>
     <link rel="stylesheet" href="style.css"/>
+    <link rel="shortcut icon" href="favicon.png"/>
 </head>
 <body>
 <?php
-foreach ($game['objects'] as $object_id => $object) {
-    $object['on_fire'] = (bool)rand(true, false);
-    $object['is_target'] = !$object['on_fire'];
-    $game['objects'][$object_id] = $object;
-}
-?>
+//foreach ($game['objects'] as $object_id => $object) {
+//    $object['on_fire'] = (bool)rand(true, false);
+//    $object['is_target'] = !$object['on_fire'];
+//    $game['objects'][$object_id] = $object;
+//}
+//?>
+<div class="game-screen">
 <?php foreach ($game['objects'] as $object_id => $object): ?>
     <div id="ob-<?= $object_id; ?>" class="<?= $object['class']; ?> object"
          style="top: <?= $object['x']; ?>px; left: <?= $object['y']; ?>px;">
         <div class="target"></div>
-
-
     </div>
 <?php endforeach; ?>
-
+<div class="score">0 / 3</div>
+</div>
 <audio id="audio_shot">
     <source src="shot.mp3" type="audio/mpeg">
 </audio>
--- foreach/script.js
+++ foreach/script.js
@@ -1,6 +1,9 @@
 const object = document.querySelectorAll(".object")
+const score = document.querySelector(".score")
+let targets_burned = 0
 addEventListener('click', ob => {
         if (ob.path[0].classList.contains("object")) {
+            const targets = ob.path.length - 3
             document.getElementById("audio_shot").play();
             setTimeout(() => {
                 document.getElementById("audio_explosion").play();
@@ -9,11 +12,22 @@ addEventListener('click', ob => {
             const id = document.querySelector(selector)
             id.classList.remove("target")
             id.classList.add("on-fire")
+            score.classList.toggle("score-add")
+            setTimeout(() => {
+                score.classList.toggle("score-add")
+            }, 300);
             setTimeout(() => {
                 id.classList.remove("on-fire")
                 id.classList.add("burned");
                 ob.path[0].classList.add("burned")
             }, 1500);
+            targets_burned += 1
+            if (targets_burned === targets) {
+                score.classList.toggle("score-win")
+                score.innerHTML = "MISSION PASSED !"
+            } else {
+                score.innerHTML = targets_burned + '/' + targets;
+            }
         } else {
             // MISSED COMMANDS
             document.getElementById("audio_shot").play();
